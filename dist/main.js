f4179012a8f7ef5253bc40c1638f117b98486b34
pick-cee <akinloluwaolumuyide@gmail.com>
2021-09-01 16:55:25 +0100

    updates

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
+    const APP_VERSION = process.env.npm_package_version;
+    const options = new swagger_1.DocumentBuilder()
+        .setTitle('HAVISM')
+        .setDescription('API for havissm')
+        .setVersion(APP_VERSION)
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, options);
+    swagger_1.SwaggerModule.setup('swagger', app, document);
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  //const APP_NAME = process.env.npm_package_name;
+  const APP_VERSION = process.env.npm_package_version;
+
+  const options = new DocumentBuilder()
+    .setTitle('HAVISM')
+    .setDescription('API for havissm')
+    .setVersion(APP_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
